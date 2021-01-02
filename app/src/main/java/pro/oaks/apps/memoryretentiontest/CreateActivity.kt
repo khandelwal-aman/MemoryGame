57adf5e759134603882f6b1bb447790276c37dd5
Aman Khandelwal <amank@oakstree.com>
2021-01-02 16:42:39 +0530

    create activity added

---
+++ app/src/main/java/pro/oaks/apps/memoryretentiontest/CreateActivity.kt
@@ -0,0 +1,31 @@
+package pro.oaks.apps.memoryretentiontest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import pro.oaks.apps.memoryretentiontest.models.BoardSize
+import pro.oaks.apps.memoryretentiontest.utils.EXTRA_BOARD_SIZE
+
+class CreateActivity : AppCompatActivity() {
+
+    private lateinit var boardSize:BoardSize
+    private var numImagesRequired = -1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        boardSize = intent.getSerializableExtra(EXTRA_BOARD_SIZE) as BoardSize
+        numImagesRequired = boardSize.getNumPairs()
+        supportActionBar?.title = "Choose pics (0 / $numImagesRequired)"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home){
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
